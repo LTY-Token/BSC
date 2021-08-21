902d76b721cda902423389160b1bd5d775ee5e34
Oleh MISAROSH <oleh.misarosh@1000geeks.com>
2021-08-22 02:48:49 +0300

    Deploy to testnet

--- env.example.ts
+++ env.example.ts
@@ -1,1 +1,2 @@
 export const TEST_PRIVATE_KEY = '';
+export const BSCSCAN_API_KEY = '';
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import '@typechain/hardhat';
 import 'hardhat-deploy';
 import { HardhatUserConfig, task } from 'hardhat/config';
 import 'solidity-coverage';
-import { TEST_PRIVATE_KEY } from './env';
+import { BSCSCAN_API_KEY, TEST_PRIVATE_KEY } from './env';
 
 function typedNamedAccounts<T>(namedAccounts: { [key in string]: T }) {
   return namedAccounts;
@@ -27,21 +27,27 @@ const config: HardhatUserConfig = {
       chainId: 1337,
       accounts: [TEST_PRIVATE_KEY],
     },
+    bsctestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      accounts: [TEST_PRIVATE_KEY],
+    }
   },
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: "TVHC8TX5TTYPBQFQ5E9GWT8QN74CSK5T3H",
+    apiKey: BSCSCAN_API_KEY,
   },
   namedAccounts: typedNamedAccounts({
     deployer: {
       private: 0,
+      bsctestnet: 0,
     },
     pancakeswapRouter: {
       private: '0xDbba2DF274ED3fD1350f5D18F85148Fb01fAbbfC',
+      bsctestnet: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     },
     usdc: {
       private: '0xFF7F978a9e591C2a870aEc1fD0876f7FF8f3036e',
+      bsctestnet: '0xcac913Ab86ef470781b5300ADC6720540838fac2',
     },
   }),
   typechain: {
